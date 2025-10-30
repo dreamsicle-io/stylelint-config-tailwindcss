c41dd9c78fe73ce2820eee84c8bb021f7339961b
Noah Mason <noah@dreamsicle.io>
2025-10-30 17:42:13 -0400

    Moves template to src, and imports dist in root level stylelint config for testing.

---
+++ src/stylelint.config.mjs
@@ -0,0 +1,62 @@
+
+// @ts-check
+
+import types from "../syntax/types.json";
+import properties from "../syntax/properties.json";
+import atRules from "../syntax/at-rules.json";
+
+/**
+ * @type {import("stylelint").Config}
+ */
+const stylelintConfig = {
+	languageOptions: {
+		syntax: {
+			types: {
+				"tailwindcss-color": "<color> | <--alpha()>",
+				"tailwindcss-length": "<length> | <--spacing()>",
+				"--alpha()": "--alpha( <color> / <percentage> )",
+				"--spacing()": "--spacing( <number> )",
+				...types,
+			},
+			properties: {
+				...properties,
+			},
+			atRules: {
+				"theme": {
+					prelude: "inline",
+				},
+				"source": {
+					prelude: "<string>",
+				},
+				"utility": {
+					prelude: "<custom-ident>",
+				},
+				"variant": {
+					prelude: "<custom-ident>",
+				},
+				"custom-variant": {
+					prelude: "<custom-ident>",
+				},
+				"reference": {
+					prelude: "<string>",
+				},
+				"apply": {
+					prelude: "<custom-ident>+",
+				},
+				...atRules,
+			},
+		},
+	},
+	rules: {
+		"import-notation": "string",
+		"lightness-notation": "number",
+		"hue-degree-notation": "number",
+		"nesting-selector-no-missing-scoping-root": [true, {
+			ignoreAtRules: [
+				"custom-variant",
+			],
+		}],
+	},
+};
+
+export default stylelintConfig;
